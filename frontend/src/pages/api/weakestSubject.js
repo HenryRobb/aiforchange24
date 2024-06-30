fcf04b5a928b8f1204e4b3c9cf6710861de3f8f1
AidanWestphal <kylesteine1@gmail.com>
2024-06-30 11:12:38 -0400

    Local Storage for User Credentials and Login/Logout Functions

--- frontend/src/pages/api/weakestSubject.js
+++ frontend/src/pages/api/weakestSubject.js
@@ -5,22 +5,16 @@ import mongoose from "mongoose";
 // Returns the weakest subject within a given course for a user.
 export default async function handler(req, res) {
     await connectDB()
-    
-    const { username, password, course } = req.body;
-    const bcrypt = require ('bcrypt');
-    let hash_pass = password;
-    bcrypt.hash(password, saltRounds, function (err, hash) {
-        if (err) {
-            res.status(200).json({ done: false, message: "Password hashing failed." });
-        }
-        hash_pass = hash;
-    });
+    const { course } = req.body;
+    const username = localStorage.getItem("username", username);
+    const hash_pass = localStorage.getItem("password", hash_pass);
+
     const Person = mongoose.model('User', user);
 
     const user = await Person.find().byName(username).exec();
-    if(user.password === hash_pass) {
-        res.status(200).json({ success: true, subject: user.findWeakestSubject(course), message: "Success."});
-    } else {
-        res.status(200).json({ success: false, message: "Either the username or password was incorrect." });
+    if (!user.password === hash_pass) {
+        res.status(200).json({ success: true, courseHistory: user.courseHistory, message: "Either the username or password was incorrect." });
     }
+    res.status(200).json({ success: true, subject: user.findWeakestSubject(course), message: "Success." });
+
 }
